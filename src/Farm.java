4f178074624fcafa162d512e4fc6941e5cff8d9c
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:24:46 +0100

    created new method addAnimal

--- src/Farm.java
+++ src/Farm.java
@@ -11,5 +11,22 @@ public class Farm {
     private ArrayList<Animal> bigAnimals = new ArrayList<>();
 
 
+    public void addAnimal(Animal animal){
+        switch (animal.size){
+            case BIG:
+                if(smallAnimals.size() <= 10){
+                    smallAnimals.add(animal);
+                    animals[animals.length-1] = animal;
+                }
+            case SMALL:
+                if(bigAnimals.size() <= 10){
+                    bigAnimals.add(animal);
+                    animals[animals.length-1] = animal;
+                }
+        }
+
+    }
+
+
 
 }
