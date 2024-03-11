f901c8e3beba54a53c34a68f6d0966412cfb829c
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:28:53 +0100

    created new class Cow

---
+++ src/Tulip.java
@@ -0,0 +1,7 @@
+public class Tulip extends Flower{
+
+    public Tulip(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+
+}
