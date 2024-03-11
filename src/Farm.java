0cef601d5e35f122b18a9929872ddfa62800c2e6
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:05:18 +0100

    created new "vlastnost" bigAnimals

--- src/Farm.java
+++ src/Farm.java
@@ -4,7 +4,11 @@ public class Farm {
 
     private ArrayList<Flower> flowers = new ArrayList<>();
 
-    private ArrayList<Animal> animals = new ArrayList<>();
+    private Animal[] animals = new Animal[20];
+
+    private ArrayList<Animal> smallAnimals = new ArrayList<>();
+
+    private ArrayList<Animal> bigAnimals = new ArrayList<>();
 
 
 
