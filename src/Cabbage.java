551b9723df3fdaa95e00bdf4938f6df19ea719a2
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:46:15 +0100

    created new class Cabbage

---
+++ src/Cabbage.java
@@ -0,0 +1,8 @@
+public class Cabbage extends Flower{
+
+    private final FlowerType flowerType = FlowerType.CABBAGE;
+
+    public Cabbage(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
