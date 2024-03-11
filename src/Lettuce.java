224c48103c6eea5738d8c983762685660854560d
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:46:26 +0100

    created new class Lettuce

---
+++ src/Lettuce.java
@@ -0,0 +1,8 @@
+public class Lettuce extends Flower{
+
+    private final FlowerType flowerType = FlowerType.LETTUCE;
+
+    public Lettuce(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
