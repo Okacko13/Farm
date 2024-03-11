1141258a9cf174d3169a8c85b4c9c4cd67e6e1b1
krejci3 <ondra.krejci13@gmail.com>
2024-03-11 10:46:37 +0100

    created new class Sunflower

---
+++ src/Sunflower.java
@@ -0,0 +1,8 @@
+public class Sunflower extends Flower{
+
+    private final FlowerType flowerType = FlowerType.SUNFLOWER;
+
+    public Sunflower(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
